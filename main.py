6601e18cc976a4d4fb5a34cdf132502bc96be54d
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2023-05-13 15:40:42 -0300

    atualizacao2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import psycopg2
 import os
 
 
@@ -6,10 +7,30 @@ app = Flask(__name__)
 
 port = int(os.environ.get('PORT', 5000))
 
+db_name = "wms_bd"
+db_user = "wms"
+db_password = "Master100"
+db_host = "wmsbd.cyiuowfro4wv.sa-east-1.rds.amazonaws.com"
+portbanco = "5432"
+
+
+conn = psycopg2.connect(database=db_name, user=db_user, password=db_password, host=db_host, port= portbanco)
+cursor = conn.cursor()
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
+@app.route('/api/Filaeposicao', methods=['GET'])
+def get_usuarios():
+    cursor.execute('select * from "Reposicao"."FilaReposicaoTags" frt ')
+    Filaeposicao = cursor.fetchall()
+    return jsonify(Filaeposicao)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
+    cursor.close()
+    conn.close()
