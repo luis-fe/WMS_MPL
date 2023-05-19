6c60b199078083982915b96628d6a16d759d2063
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2023-05-19 15:03:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -198,6 +198,23 @@ def get_AtribuirOPRepositor():
         # Retorna uma resposta de sucesso
         return jsonify({'message': True})
     
+@app.route('/api/DetalhaOP', methods=['GET'])
+@token_required
+def get_DetalhaOP():
+    # Obtém o código do usuário e a senha dos parâmetros da URL
+    NumeroOP = request.args.get('numeroOP')
+    op = OPfilaRepor.detalhaOP(NumeroOP)
+    # Obtém os nomes das colunas
+    column_names = op.columns
+    # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
+    OP_data = []
+    for index, row in op.iterrows():
+        op_dict = {}
+        for column_name in column_names:
+            op_dict[column_name] = row[column_name]
+        OP_data.append(op_dict)
+    return jsonify(OP_data)    
+    
 
     
 if __name__ == '__main__':
