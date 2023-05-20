5c1dad774670075fa79bfc0b600a703974d64cf6
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2023-05-20 09:05:40 -0300

    Update OPfilaRepor.py

--- OPfilaRepor.py
+++ OPfilaRepor.py
@@ -4,11 +4,15 @@ import numpy
 
 def FilaPorOP():
     conn = ConecaoAWSRS.conexao()
-    df_OP1 = pd.read_sql(' select "numeroOp", count("numeroOp")as QtdPeçs_total  from "Reposicao"."FilaReposicaoporTag" frt ' 
-                        '  group by "numeroOp" ',conn)
+    df_OP1 = pd.read_sql(' select "numeroOp", count("numeroOp")as QtdPeçs_total, "Usuario" as codusuario_atribuido  from "Reposicao"."FilaReposicaoporTag" frt ' 
+                        '  group by "numeroOp", "Usuario"  ',conn)
     df_OP_Iniciada =pd.read_sql(' select "numeroOp", count("numeroOp") as QtdPeçs_Reposto  from "Reposicao"."FilaReposicaoporTag" frt ' 
                         '  where "Situacao" = '+ "'nao iniciada'"+ ' group by "numeroOp" ',conn)
     df_OP1 = pd.merge(df_OP1,df_OP_Iniciada,on='numeroOp',how='left')
+    usuarios = pd.read_sql(
+        'select codigo as codusuario_atribuido , nome as nomeusuario_atribuido  from "Reposicao".cadusuarios c ', conn)
+    usuarios['codusuario_atribuido'] = usuarios['codusuario_atribuido'].astype(str)
+    df_OP1 = pd.merge(df_OP1, usuarios, on='codusuario_atribuido', how='left')
     df_OP1['qtdpeçs_reposto'] = df_OP1['qtdpeçs_reposto'].replace('', numpy.nan).fillna('0')
     df_OP1['qtdpeçs_total'] = df_OP1['qtdpeçs_total'].astype(int)
     df_OP1['qtdpeçs_reposto'] = df_OP1['qtdpeçs_reposto'].astype(int)
@@ -16,9 +20,11 @@ def FilaPorOP():
     # Clasificando o Dataframe para analise
     df_OP1 = df_OP1.sort_values(by='qtdpeçs_total', ascending=False, ignore_index=True)  # escolher como deseja classificar
     df_OP1["Situacao"] = df_OP1.apply(lambda row: 'Iniciada'  if row['qtdpeçs_reposto'] >0 else 'Nao Iniciada', axis=1)
+    df_OP1['codusuario_atribuido'] = df_OP1['codusuario_atribuido'].replace('', numpy.nan).fillna('-')
+    df_OP1['nomeusuario_atribuido'] = df_OP1['nomeusuario_atribuido'].replace('', numpy.nan).fillna('-')
+    conn.close()
     # Limitar o número de linhas usando head()
-    df_OP1 = df_OP1.head(50) 
-
+    df_OP1 = df_OP1.head(50)  # Retorna as 3 primeiras linhas
     return df_OP1
 
 def AtribuiRepositorOP(codigo, numeroOP):
