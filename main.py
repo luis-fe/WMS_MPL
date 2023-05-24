09071f692950fb43375b3ad53ca8f850c6c643aa
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2023-05-24 12:06:07 -0300

    detalhaOPxSKU

--- main.py
+++ main.py
@@ -216,6 +216,23 @@ def get_DetalhaOP():
         OP_data.append(op_dict)
     return jsonify(OP_data)
 
+@app.route('/api/DetalhaOPxSKU', methods=['GET'])
+@token_required
+def get_DetalhaOPxSKU():
+    # Obtém o código do usuário e a senha dos parâmetros da URL
+    NumeroOP = request.args.get('numeroOP')
+    op = OPfilaRepor.detalhaOPxSKU(NumeroOP)
+    # Obtém os nomes das colunas
+    column_names = op.columns
+    # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
+    OP_data = []
+    for index, row in op.iterrows():
+        op_dict = {}
+        for column_name in column_names:
+            op_dict[column_name] = row[column_name]
+        OP_data.append(op_dict)
+    return jsonify(OP_data)
+
 @app.route('/api/Enderecos', methods=['GET'])
 @token_required
 def get_enderecos():
