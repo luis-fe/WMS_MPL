b548f39433c7a19bd4363b27066e1698a30262e5
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2023-05-24 19:58:48 -0300

    Update OPfilaRepor.py

--- OPfilaRepor.py
+++ OPfilaRepor.py
@@ -18,7 +18,7 @@ def FilaPorOP():
     df_OP1['qtdpeçs_total'] = df_OP1['qtdpeçs_total'].replace('', numpy.nan).fillna('0')
     df_OP1['qtdpeçs_total'] = df_OP1['qtdpeçs_total'].astype(int)
     df_OP1['qtdpeçs_reposto'] = df_OP1['qtdpeçs_reposto'].astype(int)
-    df_OP1['% Reposto'] = numpy.divide(df_OP1['qtdpeçs_total']-df_OP1['qtdpeçs_aRepor'],df_OP1['qtdpeçs_total'])
+    df_OP1['% Reposto'] = 1 - numpy.divide(df_OP1['qtdpeçs_aRepor'],df_OP1['qtdpeçs_total'])
     df_OP1['% Reposto'] = df_OP1['% Reposto'].round(2)*100
     # Clasificando o Dataframe para analise
     df_OP1 = df_OP1.sort_values(by='qtdpeçs_total', ascending=False, ignore_index=True)  # escolher como deseja classificar
