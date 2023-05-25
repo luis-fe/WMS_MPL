1d0b0437e8feede3d8ecedab77b29da9039cc831
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2023-05-25 14:21:14 -0300

    Update UsuariosAWSRS.py

--- UsuariosAWSRS.py
+++ UsuariosAWSRS.py
@@ -2,6 +2,7 @@ import ConecaoAWSRS
 import pandas as pd
 
 
+
 def PesquisarUsuarios():
     conn = ConecaoAWSRS.conexao()
     cursor = conn.cursor()
@@ -11,8 +12,20 @@ def PesquisarUsuarios():
     conn.close()
     return usuarios
 
+def PesquisarUsuariosCodigo(codigo):
+    conn = ConecaoAWSRS.conexao()
+    cursor = conn.cursor()
+    cursor.execute('select codigo, nome, funcao, situacao from "Reposicao"."cadusuarios" c'
+                   ' where codigo = %s',(codigo,))
+    usuarios = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return usuarios[0][1],usuarios[0][2],usuarios[0][3]
+
+
+
 def AtualizarInformacoes(novo_nome, nova_funcao, nova_situacao,  codigo):
-    conn = ConexaoPostgreRailway.conexao()
+    conn = ConecaoAWSRS.conexao()
     cursor = conn.cursor()
     cursor.execute('UPDATE "Reposicao"."cadusuarios" SET nome=%s, funcao=%s, situacao= %s WHERE codigo=%s',(novo_nome,nova_funcao,nova_situacao, codigo))
     conn.commit()
@@ -20,13 +33,3 @@ def AtualizarInformacoes(novo_nome, nova_funcao, nova_situacao,  codigo):
     conn.close()
     return novo_nome
 
-
-
-def AtualizarInformacoes(novo_nome, nova_funcao, nova_situacao,  codigo):
-    conn = ConexaoPostgreRailway.conexao()
-    cursor = conn.cursor()
-    cursor.execute('UPDATE "Reposicao"."cadusuarios" SET nome=%s WHERE codigo=%s',(novo_nome, codigo))
-    conn.commit()
-    cursor.close()
-    conn.close()
-    return novo_nome
