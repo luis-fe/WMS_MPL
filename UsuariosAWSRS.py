6813e37ce90e136a10a3cac657976c440151b070
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2023-05-25 14:00:21 -0300

    Update UsuariosAWSRS.py

--- UsuariosAWSRS.py
+++ UsuariosAWSRS.py
@@ -10,3 +10,24 @@ def PesquisarUsuarios():
     cursor.close()
     conn.close()
     return usuarios
+
+def PesquisarUsuariosCodigo(codigo):
+    conn = ConexaoPostgreRailway.conexao()
+    cursor = conn.cursor()
+    cursor.execute('select codigo, nome, funcao, situacao from "Reposicao"."cadusuarios" c'
+                   ' where codigo = %s',(codigo,))
+    usuarios = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return usuarios[0][1],usuarios[0][2],usuarios[0][3]
+
+
+
+def AtualizarInformacoes(novo_nome, nova_funcao, nova_situacao,  codigo):
+    conn = ConexaoPostgreRailway.conexao()
+    cursor = conn.cursor()
+    cursor.execute('UPDATE "Reposicao"."cadusuarios" SET nome=%s WHERE codigo=%s',(novo_nome, codigo))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return novo_nome
