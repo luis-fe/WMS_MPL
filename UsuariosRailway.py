a8bb9daab804e4f9a70a8200142016e8512e2521
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2023-05-25 14:20:53 -0300

    Update UsuariosRailway.py

--- UsuariosRailway.py
+++ UsuariosRailway.py
@@ -2,6 +2,7 @@ import ConexaoPostgreRailway
 import pandas as pd
 
 
+
 def PesquisarUsuarios():
     conn = ConexaoPostgreRailway.conexao()
     cursor = conn.cursor()
@@ -11,21 +12,22 @@ def PesquisarUsuarios():
     conn.close()
     return usuarios
 
+def PesquisarUsuariosCodigo(codigo):
+    conn = ConexaoPostgreRailway.conexao()
+    cursor = conn.cursor()
+    cursor.execute('select codigo, nome, funcao, situacao from "Reposicao"."cadusuarios" c'
+                   ' where codigo = %s',(codigo,))
+    usuarios = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return usuarios[0][1],usuarios[0][2],usuarios[0][3]
+
+
+
 def AtualizarInformacoes(novo_nome, nova_funcao, nova_situacao,  codigo):
     conn = ConexaoPostgreRailway.conexao()
     cursor = conn.cursor()
     cursor.execute('UPDATE "Reposicao"."cadusuarios" SET nome=%s, funcao=%s, situacao= %s WHERE codigo=%s',(novo_nome,nova_funcao,nova_situacao, codigo))
-    conn.commit()
-    cursor.close()
-    conn.close()
-    return novo_nome
-
-
-
-def AtualizarInformacoes(novo_nome, nova_funcao, nova_situacao,  codigo):
-    conn = ConexaoPostgreRailway.conexao()
-    cursor = conn.cursor()
-    cursor.execute('UPDATE "Reposicao"."cadusuarios" SET nome=%s WHERE codigo=%s',(novo_nome, codigo))
     conn.commit()
     cursor.close()
     conn.close()
