4c96900bc825d565b5746f350e46f82eed4585df
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2023-05-25 11:33:05 -0300

    Create UsuariosAWSRS.py

---
+++ UsuariosAWSRS.py
@@ -0,0 +1,12 @@
+import ConecaoAWSRS
+import pandas as pd
+
+
+def PesquisarUsuarios():
+    conn = ConecaoAWSRS.conexao()
+    cursor = conn.cursor()
+    cursor.execute('select codigo, nome, funcao, situacao from "Reposicao"."cadusuarios" c')
+    usuarios = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return usuarios
