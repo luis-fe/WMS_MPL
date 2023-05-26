dd7ac6850d012ffc5e2f5255002ff2f9cc8bbfbb
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2023-05-26 11:48:38 -0300

    Update ReposicaoRailway.py

--- ReposicaoRailway.py
+++ ReposicaoRailway.py
@@ -21,3 +21,43 @@ def PesquisaEndereco(endereco):
     else:
 
         return pd.DataFrame({'Status': [True], 'Mensagem': [f'endereco {endereco} encontrado!']})
+    
+def SituacaoEndereco(endereco):
+    conn = ConexaoPostgreRailway.conexao()
+    select = 'select * from "Reposicao"."cadendereco" ce ' \
+             'where codendereco = %s'
+    cursor = conn.cursor()
+    cursor.execute(select, (endereco, ))
+    resultado = cursor.fetchall()
+    cursor.close()
+    if not resultado:
+        conn.close()
+        return pd.DataFrame({'Status Endereco': [False], 'Mensagem': [f'endereco {endereco} nao existe!']})
+    else:
+        saldo = Estoque_endereco(endereco)
+        if saldo == 0:
+            conn.close()
+            return pd.DataFrame({'Status Endereco': [True], 'Mensagem': [f'endereco {endereco} existe!'],
+                                 'Status do Saldo': ['Vazio']})
+        else:
+            skus = pd.read_sql('select  "codreduzido", "Saldo"  from "Reposicao"."estoque" e '
+                                    'where "endereco"='+" '"+endereco+"'",conn)
+            conn.close()
+            skus['enderco'] = endereco
+            skus['Status Endereco'] = True
+            skus['Mensagem'] = f'Endereço {endereco} existe!'
+            skus['Status do Saldo']='Cheio'
+            return skus
+
+def Estoque_endereco(endereco):
+    conn = ConexaoPostgreRailway.conexao()
+    consultaSql = 'select "Saldo" from "Reposicao"."estoque" e ' \
+                  'where "endereco" = %s'
+    cursor = conn.cursor()
+    cursor.execute(consultaSql, (endereco,))
+    resultado = cursor.fetchall()
+    cursor.close()
+    if not resultado:
+        return 0
+    else:
+        return resultado[0][0]
