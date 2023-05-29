30b6e375eecbf36af273c2578450b831fa644c78
luis-fe <luis_engp@hotmail.com>
2023-05-29 15:46:48 -0300

    atualizacao2

--- ReposicaoRailway.py
+++ ReposicaoRailway.py
@@ -70,7 +70,7 @@ def Devolver_Inf_Tag(codbarras):
         'where "codbarrastag" = '+"'"+codbarras+"'", conn)
     TagApontadas = pd.read_sql('select count("codbarrastag") as situacao, "codReduzido", "numeroop"  from "Reposicao"."tagsreposicao" tr '
                                'where"codbarrastag" = '+"'"+codbarras+"'"+
-                               ' group by "codbarrastag" ',conn)
+                               ' group by "codbarrastag","codReduzido", "numeroop"  ',conn)
 
     conn.close()
     if not TagApontadas.empty and TagApontadas["situacao"][0] >= 0:
