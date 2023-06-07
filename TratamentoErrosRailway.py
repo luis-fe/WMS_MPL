5e0f8f6311d867eee478073ed7215b058276023d
luis-fe <luis_engp@hotmail.com>
2023-06-07 12:09:09 -0300

    TratamentoErro1

--- TratamentoErrosRailway.py
+++ TratamentoErrosRailway.py
@@ -5,16 +5,101 @@ import numpy
 def VerificarDuplicacoesDeTagsFila():
     conn = ConexaoPostgreRailway.conexao()
     # VERIFICANDO SE EXISTE CODIGO DE BARRAS DUPLICADOS NA FILA
-    erro1 = pd.read_sql('select * from "Reposicao".filareposicaoportag f '
-                        'where codbarrastag in'
-                        ' (select codbarrastag  from ( '
-                        ' select count(codbarrastag) as conta, codbarrastag  from "Reposicao".filareposicaoportag f '
+    erro1 = pd.read_sql('select cont from '
+                        '(select count(codbarrastag) as cont  from "Reposicao".filareposicaoportag f '
                         'group by codbarrastag) as contagem '
-                        'where contagem.conta > 1)', conn)
+                        'where contagem.cont >1', conn)
     conn.close()
     if erro1.empty:
-        return False
+
+        return pd.DataFrame({'Mensagem': [f'Nao tem tag duplicada na fila']})
     else:
-        return True
+        return pd.DataFrame({'Mensagem': [f' Atencao  tem tag duplicada na fila!!!']})
+
+def VerificarDuplicacoesTagReposta():
+    conn = ConexaoPostgreRailway.conexao()
+    # VERIFICANDO SE EXISTE CODIGO DE BARRAS DUPLICADOS NA FILA
+    erro1 = pd.read_sql('select cont from '
+                        '(select count(codbarrastag) as cont  from "Reposicao".tagsreposicao f '
+                        'group by codbarrastag) as contagem '
+                        'where contagem.cont >1', conn)
+    conn.close()
+    if erro1.empty:
+
+        return pd.DataFrame({'Mensagem': [f'Nao tem tag duplicada reposta']})
+    else:
+        return pd.DataFrame({'Mensagem': [f' Atencao  tem tag duplicada reposta!!!']})
+
+def VerificarDuplicacoesTagRepostaInventario():
+    conn = ConexaoPostgreRailway.conexao()
+    # VERIFICANDO SE EXISTE CODIGO DE BARRAS DUPLICADOS NA FILA
+    erro1 = pd.read_sql('select cont from '
+                        '(select count(codbarrastag) as cont  from "Reposicao".tagsreposicao_inventario f '
+                        'group by codbarrastag) as contagem '
+                        'where contagem.cont >1', conn)
+    conn.close()
+    if erro1.empty:
+
+        return pd.DataFrame({'Mensagem': [f'Nao tem tag duplicada no inventario']})
+    else:
+        return pd.DataFrame({'Mensagem': [f' Atencao  tem tag duplicada no inventario!!!']})
 
 
+def VerificandoTagsSemelhanteFIlaxReposicao():
+    conn = ConexaoPostgreRailway.conexao()
+    # VERIFICANDO SE EXISTE CODIGO DE BARRAS DUPLICADOS NA FILA
+    erro1 = pd.read_sql('select t.codbarrastag  from "Reposicao".tagsreposicao t '
+                        'join "Reposicao".filareposicaoportag f on t.codbarrastag = f.codbarrastag  '
+                        , conn)
+    conn.close()
+
+    if erro1.empty:
+
+        return pd.DataFrame({'Mensagem': [f'Nao tem tag duplicada na fila x reposicao']})
+    else:
+        erro1['Mensagem'] = 'Tag Duplicada na Fila e no Bipada'
+        return erro1
+
+def VerificandoTagsSemelhanteReposicaoxInventario():
+    conn = ConexaoPostgreRailway.conexao()
+    # VERIFICANDO SE EXISTE CODIGO DE BARRAS DUPLICADOS NA FILA
+    erro1 = pd.read_sql('select t.codbarrastag  from "Reposicao".tagsreposicao t '
+                        'join "Reposicao".tagsreposicao_inventario f on t.codbarrastag = f.codbarrastag  '
+                        , conn)
+    conn.close()
+
+    if erro1.empty:
+
+        return pd.DataFrame({'Mensagem': [f'Nao tem tag duplicada na reposicao x inventario']})
+    else:
+        erro1['Mensagem'] = 'Tag Duplicada na reposicao e no Inventario'
+        return erro1
+
+def VerificandoTagsSemelhanteFilaxInventario():
+    conn = ConexaoPostgreRailway.conexao()
+    # VERIFICANDO SE EXISTE CODIGO DE BARRAS DUPLICADOS NA FILA
+    erro1 = pd.read_sql('select t.codbarrastag  from "Reposicao".filareposicaoportag t '
+                        'join "Reposicao".tagsreposicao_inventario f on t.codbarrastag = f.codbarrastag  '
+                        , conn)
+    conn.close()
+
+    if erro1.empty:
+
+        return pd.DataFrame({'Mensagem': [f'Nao tem tag duplicada na fila x inventario']})
+    else:
+        erro1['Mensagem'] = 'Tag Duplicada na fila e no Inventario'
+        return erro1
+
+
+def ListaErros():
+    a = VerificarDuplicacoesDeTagsFila()
+    b = VerificarDuplicacoesTagReposta()
+    c = VerificarDuplicacoesTagRepostaInventario()
+    d = VerificandoTagsSemelhanteFIlaxReposicao()
+    e = VerificandoTagsSemelhanteReposicaoxInventario()
+    f = VerificandoTagsSemelhanteFilaxInventario()
+    df_concat = pd.concat([a, b, c, d,e, f], axis=0)
+    return df_concat
+
+
+print(ListaErros())
