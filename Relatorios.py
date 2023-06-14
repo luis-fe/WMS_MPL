c4af77b117f0a2ce1649c49dbe4b783ace394e13
luis-fe <luis_engp@hotmail.com>
2023-06-14 16:40:39 -0300

    teste

--- Relatorios.py
+++ Relatorios.py
@@ -7,8 +7,8 @@ import time
 # CLASSE COM AS FUNÇOES PARA INTERAGIR COM AS APIS DE ACESSO DA "REPOSICAO"
 def relatorioEndereços ():
     conn = ConexaoPostgreRailway.conexao()
-    relatorioEndereço = pd.read_sql('select "Endereco","CodReduzido" ,"Engenharia" , count(codbarrastag) as saldo  from "Reposicao".tagsreposicao t   '
-                                    'group by "Endereco", "CodReduzido" , "Engenharia"   ',conn)
+    relatorioEndereço = pd.read_sql('select "Endereco","CodReduzido" ,"Engenharia" , count(codbarrastag) as saldo, "Descricao", cor , tamanho     from "Reposicao".tagsreposicao t   '
+                                    'group by "Endereco", "CodReduzido" , "Engenharia" ,"Descricao", cor , tamanho   ',conn)
     conn.close()
     return relatorioEndereço
 
