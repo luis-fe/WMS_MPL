437b69cd7dd9e780a788cbf4c1414514c30a216d
luis-fe <luis_engp@hotmail.com>
2023-06-16 22:45:27 -0300

    Usuario3

--- ReposicaoRailway.py
+++ ReposicaoRailway.py
@@ -235,12 +235,12 @@ def ApontarReposicao(codUsuario, codbarras, endereco, dataHora):
         return 'Reposto'
     else:
         #insere os dados da reposicao
-        Insert = ' INSERT INTO "Reposicao"."tagsreposicao" ("codbarrastag","Endereco","DataReposicao","CodReduzido","Engenharia","Descricao", ' \
+        Insert = ' INSERT INTO "Reposicao"."tagsreposicao" ("Usuario","codbarrastag","Endereco","DataReposicao","CodReduzido","Engenharia","Descricao", ' \
                  '"cor", "Epc", "tamanho" )' \
-                 ' VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s);'
+                 ' VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);'
         cursor = conn.cursor()
         cursor.execute(Insert
-                       , (codbarras, endereco,dataHora,reduzido,codEngenharia,descricao,cor,epc,tam))
+                       , (usuario, codbarras, endereco,dataHora,reduzido,codEngenharia,descricao,cor,epc,tam))
 
         # Obter o número de linhas afetadas
         conn.commit()
