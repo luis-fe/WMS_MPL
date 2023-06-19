0fb1faee00fd5e619ac183553ce2a273f3b39377
luis-fe <luis_engp@hotmail.com>
2023-06-19 09:42:29 -0300

    Refencia

--- PediosReporRailway.py
+++ PediosReporRailway.py
@@ -77,7 +77,7 @@ def DetalhaPedido(codPedido):
     DetalhaSku = pd.read_sql('select  produto as reduzido, qtdesugerida , status as concluido_X_total, endereco as endereco'
                             ' from "Reposicao".pedidossku p  where codpedido= '+"'"+codPedido+"'"
                                                                                               " order by endereco asc",conn)
-    descricaoSku = pd.read_sql( 'select f."codreduzido" as reduzido, f."descricao" , f."cor" , f.tamanho  from "Reposicao".filareposicaoportag f '
+    descricaoSku = pd.read_sql( 'select f."engenharia" as ref, f."codreduzido" as reduzido, f."descricao" , f."cor" , f.tamanho  from "Reposicao".filareposicaoportag f '
                                 'group by f."codreduzido", f.descricao , f."cor" , f.tamanho '
                                 ' union '
                                 'select t."codreduzido", t."descricao" , t.cor , t.tamanho  from "Reposicao".tagsreposicao t '
