037831fe9b5ca6b78ab7201f8b8a3eb1d327fa08
luis-fe <luis_engp@hotmail.com>
2023-06-20 12:57:42 -0300

    Relarorio totalFila

--- main.py
+++ main.py
@@ -622,20 +622,23 @@ def get_RelatorioFila():
         end_data.append(end_dict)
     return jsonify(end_data)
 @app.route('/api/RelatorioTotalFila', methods=['GET'])
+@token_required
 def get_RelatorioTotalFila():
-    # Obtém os dados do corpo da requisição (JSON)
-    Endereco_det = Relatorios.relatorioTotalFila()
+        # Obtém os dados do corpo da requisição (JSON)
 
-    # Obtém os nomes das colunas
-    column_names = Endereco_det.columns
-    # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
-    end_data = []
-    for index, row in Endereco_det.iterrows():
-        end_dict = {}
-        for column_name in column_names:
-            end_dict[column_name] = row[column_name]
-        end_data.append(end_dict)
-    return jsonify(end_data)
+
+        Endereco_det = Relatorios.relatorioTotalFila()
+        Endereco_det = pd.DataFrame(Endereco_det)
+        # Obtém os nomes das colunas
+        column_names = Endereco_det.columns
+        # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
+        end_data = []
+        for index, row in Endereco_det.iterrows():
+            end_dict = {}
+            for column_name in column_names:
+                end_dict[column_name] = row[column_name]
+            end_data.append(end_dict)
+        return jsonify(end_data)
 
 @app.route('/api/ListagemErros', methods=['GET'])
 
