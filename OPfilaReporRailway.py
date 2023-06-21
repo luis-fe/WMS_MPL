b47497689b44bb4ce8f6a52517b4dc3a30896b06
luis-fe <luis_engp@hotmail.com>
2023-06-21 10:38:01 -0300

    UsuarioRepor

--- OPfilaReporRailway.py
+++ OPfilaReporRailway.py
@@ -14,7 +14,7 @@ def ProdutividadeRepositores():
                    'from "Reposicao"."tagsreposicao" tr '
                    'group by "usuario" , substring("DataReposicao",1,10) '
                    'union '
-                   'select tr."usuario", '
+                   'select tr."usuario_rep", '
                    'count(tr."codbarrastag"), '
                    'substring("DataReposicao",1,10) as "DataReposicao", '
                    'min("DataReposicao") as min, '
