f4a3080fee25468b53e7c7d744c818727513d405
luis-fe <luis_engp@hotmail.com>
2023-06-24 09:26:22 -0300

    Reposto4

--- Incremento.py
+++ Incremento.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def DataFrameAtualizar():
     conn = ConexaoPostgreRailway.conexao()
-    dataframe = pd.read_sql('select p.codpedido ,p.produto  from "Reposicao".pedidossku p '
+    dataframe = pd.read_sql('select p.codpedido ,p.produto, n.endereco  from "Reposicao".pedidossku p '
                             'inner join "Reposicao".necessidadeendereco n on n.codpedido = p.codpedido and n.produto = p.produto '
                             " where p.endereco = 'Não Reposto'",conn)
     conn.close()
@@ -16,17 +16,11 @@ def testeAtualizacao(iteracoes):
         print(f'incremento: {i}')
         query = '''
             UPDATE "Reposicao".pedidossku p 
-            SET "endereco" = (
-                SELECT MAX(n.endereco)  
-                FROM "Reposicao".pedidossku s 
-                INNER JOIN "Reposicao".necessidadeendereco n 
-                ON n.codpedido = s.codpedido AND n.produto = s.produto 
-                WHERE s.endereco = 'Não Reposto'
-            ) 
+            SET "endereco" = %s
             WHERE p.endereco = 'Não Reposto' AND p.codpedido = %s AND p.produto = %s
         '''
 
         # Execute a consulta usando a conexão e o cursor apropriados
         cursor = conn.cursor()
-        cursor.execute(query, (dataframe['codpedido'][i], dataframe['produto'][i]))
+        cursor.execute(query, (dataframe['endereco'][i],dataframe['codpedido'][i], dataframe['produto'][i]))
         conn.commit()
