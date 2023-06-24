2105bdacefc3a40b16eba4e5653c2fc7c584655e
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2023-06-24 18:45:50 -0300

    Update Relatorios.py

--- Relatorios.py
+++ Relatorios.py
@@ -33,20 +33,24 @@ def relatorioTotalFila():
     query3 = pd.read_sql('select *, 1 as contagem from "Reposicao".pedidossku p'
                         " where endereco <> 'Não Reposto' and necessidade > 0",conn)
     Inventario = pd.read_sql('select codreduzido  from "Reposicao".tagsreposicao_inventario ti' ,conn)
+    Reposto = pd.read_sql('select codreduzido  from "Reposicao".tagsreposicao ti' ,conn)
 
     query['saldo'] = query['saldo'].sum()
     query2['contagem'] = query2['contagem'].sum()
     query3['contagem'] = query3['contagem'].sum()
     Inventario['codreduzido'] = Inventario['codreduzido'].count()
+    Reposto['codreduzido'] = Reposto['codreduzido'].count()
     total =  query3['contagem'][0] +  query2['contagem'][0]
     Percentual = query3['contagem'][0] / total
     Percentual = round(Percentual, 2) * 100
-
+    totalPecas = query["saldo"][0] + Reposto["codreduzido"][0]+Inventario["codreduzido"][0]
     conn.close()
     data = {
         '1.0':' Informacoes Gerais do Estoque',
-        '1.1-Saldo na Fila':   f'{query["saldo"][0]} pçs',
-        '1.2-Peçs em Inventario':   f'{Inventario["codreduzido"][0]} pçs',
+        '1.1-Total de Peças Nat. 5':   f'{totalPecas} pçs',
+        '1.2-Saldo na Fila':   f'{query["saldo"][0]} pçs',
+        '1.3-Peçs em Inventario':   f'{Reposto["codreduzido"][0]} pçs',
+        '1.4-Peçs em Inventario':   f'{Inventario["codreduzido"][0]} pçs',
         '2.0':' Informacoes dos pedidos',
         '2.1- Total de Skus nos Pedidos em aberto ': f'{total} pçs',
         '2.2-Qtd de Enderecos Nao Reposto em Pedido': f'{query2["contagem"][0]}',
